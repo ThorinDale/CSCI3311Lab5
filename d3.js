df96d2d475b91fca8b4b136e5f5bb801114d8315
Darren Zheng <Yiruma@Darrens-MacBook-Pro.local>
2020-10-20 23:09:13 -0400

    fix svg, add title

--- d3.js
+++ d3.js
@@ -3,8 +3,8 @@
 // create svg with margin convention
 const margin = ({top: 40, right: 40, bottom: 40, left: 40});
 
-const width = 700 - margin.left - margin.right;
-const height = 700 - margin.top - margin.bottom;
+const width = 600 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
 const svg = d3.select('.chart')
     .append('svg')
     .attr('width', width + margin.left + margin.right)
